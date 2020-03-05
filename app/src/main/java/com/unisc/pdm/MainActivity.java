2bb546ab20db1adcf653a1de18af12259f4694a5
Artur de Fraga Milano <m80975@acad.unisc.br>
2020-03-04 21:01:07 -0300

    commit projeto base

---
+++ app/src/main/java/com/unisc/pdm/MainActivity.java
@@ -0,0 +1,23 @@
+package com.unisc.pdm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.d(TAG, "onStart: ");
+        
+        
+    }
+}
